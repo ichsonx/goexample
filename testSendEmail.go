181c5cf2f7d8a8f3de8055ec694e37086f167c7e
ichsonx <sonxz@qq.com>
2018-03-05 16:44:52 +0800

    update

--- testSendEmail.go
+++ testSendEmail.go
@@ -10,6 +10,7 @@ import (
 	"net/smtp"
 	"strings"
 	"fmt"
+	"gopkg.in/gomail.v2"
 )
 
 var (
@@ -17,13 +18,35 @@ var (
 	user = "sonxz@qq.com"
 	host = "smtp.qq.com"
 	to = []string{"104024786@qq.com"}
+	port = 465
 )
 
 func main() {
+	SendEMailByGomail()
 }
 
+//使用第三方库gomail，
+// 缺点1：版本管理混乱，分别在github和gopkg.in都有代码和文档，而且文档中有的部分方法在引入代码后，根本就没有，证明作者没有版本更新代码
+// 缺点2：不允许多个cc或者bcc，已经查看源代码验证了
 func SendEMailByGomail()  {
+	m := gomail.NewMessage()
+	m.SetHeader("From", user)
+	m.SetHeader("To", "sonxz@qq.com")
+	//查看了源码，无论是cc还是bcc，都只允许1个地址
+	//m.SetAddressHeader("Cc", "aaa@qq.com", "")
+	//m.SetHeader("Cc", m.FormatAddress("<sonxz@qq.com>, <aaa@qq.com>", "收件人")) //抄送
+	m.SetHeader("Bcc",m.FormatAddress("<sonxz@qq.com>, <aaa@qq.com>", "收件人")) //暗送
+	m.SetHeader("Subject", "test!")
+	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+	//m.Attach("/home/Alex/lolcat.jpg")
 
+	d := gomail.NewPlainDialer(host, port, user, pw)
+	d.SSL = true
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
 }
 
 //使用golang的原生库smtp来发送邮件
