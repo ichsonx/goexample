7fbbb1a971ccd2b2fdc2d100d789ae544225d0fa
ichsonx <sonxz@qq.com>
2018-03-19 17:26:17 +0800

    add golang call phantomjs example

--- testCallPhantomjs.go
+++ testCallPhantomjs.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	p "github.com/benbjohnson/phantomjs"
 	"fmt"
+	p "github.com/benbjohnson/phantomjs"
 	"os"
+	"os/exec"
 )
 
 func main() {
+
+}
+
+func useCurl() {
+	var output []byte
+	var err error
+	// 格式化获得进程列表，还有LIST, TABLE
+	cmd := exec.Command("curl", "http://w3.afulyu.info/pw/thread.php?fid=22&page=5")
+	if output, err = cmd.Output(); err != nil {
+		fmt.Print(err)
+		os.Exit(0)
+	}
+	fmt.Println(output)
+}
+
+func usePhantomjs() {
 	if err := p.DefaultProcess.Open(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -21,7 +38,7 @@ func main() {
 
 	if err := page.Open("https://www.google.com"); err != nil {
 		fmt.Println(err)
-	}else {
+	} else {
 		fmt.Println(page.Content())
 	}
 }
