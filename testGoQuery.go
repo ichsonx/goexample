a94acc674e40cdee52e712abd8027388195d00c9
ichsonx <sonxz@qq.com>
2018-03-22 15:52:36 +0800

    add golang call phantomjs example

--- testGoQuery.go
+++ testGoQuery.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"fmt"
+)
 
+func main() {
+	doc, err := goquery.NewDocument("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		band := s.Find("a").Text()
+		title := s.Find("i").Text()
+		fmt.Printf("Review %d: %s - %s\n", i, band, title)
+	})
 }
+
