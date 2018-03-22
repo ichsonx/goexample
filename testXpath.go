81889707407b8ab87971d7cf94ddfd834c4ffcb0
ichsonx <sonxz@qq.com>
2018-03-22 17:08:13 +0800

    add golang call phantomjs example

--- testXpath.go
+++ testXpath.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"os"
+	"fmt"
+	"github.com/antchfx/htmlquery"
+)
 
+func main() {
+	f, err := os.Open(`tmp.html`)
+	if err != nil {
+		panic(err)
+	}
+	// Parse HTML document
+	doc, err := htmlquery.Parse(f)
+	if err != nil{
+		panic(err)
+	}
+
+	// List all matches nodes with the name `a`.
+	for _, n := range htmlquery.Find(doc, "//tr[@class=\"tr3 t_one\"]/td/h3/a") {
+		fmt.Printf("%s \n", htmlquery.InnerText(n))
+	}
 }
