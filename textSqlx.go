9f27329950dc0d1bfa0e6658393b6a5a769ecb67
ichsonx <sonxz@qq.com>
2018-06-29 14:40:09 +0800

    sqlx mysql test

--- textSqlx.go
+++ textSqlx.go
@@ -1,21 +1,34 @@
 package main
 
-
 import (
-	"log"
+	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"log"
 )
 
 type content struct {
-	Id string `db:"content_id"`
-	txt string `db:"txt"`
+	Id   int            `db:"content_id"`
+	Txt  string         `db:"txt"`
+	Txt1 sql.NullString `db:"txt1"`
+	Txt2 sql.NullString `db:"txt2"`
+	Txt3 sql.NullString `db:"txt3"`
 }
 
 func main() {
 	//_, err := sqlx.Connect("mysql", "user=root passowrd=cerx123 dbname=ceex ")
-	_, err := sqlx.Open("mysql", "root:cerx123@tcp(127.0.0.1:3306)/ceex")
+	db, err := sqlx.Open("mysql", "root:cerx123@tcp(127.0.0.1:3306)/ceex")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	txts := []content{}
+	err = db.Select(&txts, "SELECT * FROM jc_content_txt")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	//one := txts[0]
+	fmt.Println(len(txts))
 }
